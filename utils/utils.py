d9e1af55091861697d4cc80d3245b7b8d0ff866e
Brandon Cui <bcui19@stanford.edu>
2018-02-04 23:42:07 -0800

    Updating harness code

--- utils/utils.py
+++ utils/utils.py
@@ -10,16 +10,16 @@ Taken from:
 https://github.com/pytorch/pytorch/issues/639
 '''
 def gumbel_softmax(prob, tau = 1):
-	noise = Variable(make_gummel_noise())
-	x = (input + noise) / tau
-	x = F.softmax(x.view(input.size(0), -1))
-	return x.view_as(input)
+	noise = Variable(make_gummel_noise(prob))
+	x = (prob + noise) / tau
+	x = F.softmax(x.view(prob.size(0), -1))
+	return x.view_as(prob)
 
 '''
 Makes gummel noise as described in the paper
 '''
-def make_gummel_noise():
-	noise = torch.rand(input.size())
+def make_gummel_noise(curr_input):
+	noise = torch.rand(curr_input.size())
 	noise.add_(1e-9).log_().neg_()
 	noise.add_(1e-9).log_().neg_()
-	return noise
+	return torch.Tensor(noise)
